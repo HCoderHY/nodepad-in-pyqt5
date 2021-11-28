9aa48b2eb29d7671e1781de1a665b69a3766e451
Bogdan Tanov <86558426+BogdanTanov@users.noreply.github.com>
2021-11-28 19:35:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,110 +1,108 @@
-import pyautogui
-from PyQt5.QtWidgets import QApplication, QMenuBar, QMenu, QFileDialog
-from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtWidgets import *
-import sys
-
-class Window(QMainWindow):
-
-    def __init__(self):
-        super(Window, self).__init__()
-        self.setWindowTitle("<+NodePad->")
-        self.setStyleSheet("background-color: #252526;")
-        self.setGeometry(50,80,1800,925)
-        self.text_edit = QtWidgets.QTextEdit(self)
-        self.text_edit.setGeometry(0,45,1920,1010)
-        self.text_edit.setStyleSheet("background-color: #1C2B33; color: #CDD3D5; font-size: 15px; font-family: Noto Sans, sans-serif; border: 2px solid #252526; border-radius: 5%; margin: 5px")
-        self.createMenuBar()
-        self.pushButton = QtWidgets.QPushButton()
-
-    def createMenuBar(self):
-        self.menuBar = QMenuBar(self)
-        self.setMenuBar(self.menuBar)
-        self.menuBar.setStyleSheet("background-color: #323233; color: #8E8E8E;")
-
-        fileMenu = QMenu("&File", self)
-        self.menuBar.addMenu(fileMenu)
-        editMenu = QMenu("&Edit", self)
-        self.menuBar.addMenu(editMenu)
-
-        copy = editMenu.addAction("Copy", self.actions_clicked)
-        cut = editMenu.addAction("Cut", self.actions_clicked)
-        paste = editMenu.addAction("Paste", self.actions_clicked)
-        s_a = editMenu.addAction("Select All", self.actions_clicked)
-        u_t = editMenu.addAction("Undo Typing", self.actions_clicked)
-        redo = editMenu.addAction("Redo", self.actions_clicked)
-        delete = editMenu.addAction("Delete", self.actions_clicked)
-
-        open = fileMenu.addAction("Open", self.actions_clicked)
-        save = fileMenu.addAction("Save", self.actions_clicked)
-        exit = fileMenu.addAction("Exit", self.actions_clicked)
-
-    @QtCore.pyqtSlot()
-    def actions_clicked(self):
-        save_l = False
-        action = self.sender()
-        if action.text() == "Open":
-            fname = QFileDialog.getOpenFileName(self)[0]
-            try:
-                f = open(fname, 'r')
-                print(fname)
-                with f:
-                    data = f.read()
-                    self.text_edit.setText(data)
-            except FileNotFoundError:
-                pass
-        elif action.text() == "Save":
-            fname = QFileDialog.getSaveFileName(self)[0]
-            try:
-                f = open(fname, 'w')
-                text = self.text_edit.toPlainText()
-                f.write(text)
-                save_l = True
-            except FileNotFoundError:
-                pass
-        elif action.text() == "Exit":
-            if save_l == False:
-                save_file = QMessageBox()
-                save_file.setWindowTitle("Save file")
-                save_file.setIcon(QMessageBox.Warning)
-                save_file.setText("You want save file?")
-                save_file.setStandardButtons(QMessageBox.Ok|QMessageBox.Cancel)
-                save_file.exec_()
-                if save_file.StandardButton.Ok:
-                    fname = QFileDialog.getSaveFileName(self)[0]
-                    try:
-                        f = open(fname, 'w')
-                        text = self.text_edit.toPlainText()
-                        f.write(text)
-                    except FileNotFoundError:
-                        pass
-                elif save_file.StandardButton.Cancel:
-                    pass
-
-                sys.exit()
-
-        elif action.text() == "Undo Typing":
-            pyautogui.hotkey("ctrl", 'z')
-        elif action.text() == "Redo":
-            pyautogui.hotkey("ctrl", 'y')
-        elif action.text() == "Copy":
-            pyautogui.hotkey("ctrl", 'c')
-        elif action.text() == "Cut":
-            pyautogui.hotkey("ctrl", 'x')
-        elif action.text() == "Paste":
-            pyautogui.hotkey("ctrl", 'v')
-        elif action.text() == "Select All":
-            pyautogui.hotkey("ctrl", 'a')
-        elif action.text() == "Delete":
-            pyautogui.press("delete")
-
-
-def application():
-    app = QApplication(sys.argv)
-    window = Window()
-    window.show()
-    sys.exit(app.exec_())
-
-
-application()
+from PyQt5.QtWidgets import QApplication, QWidget, QListWidget, QPushButton, QTextEdit, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QInputDialog
+import json
+notes = {"text":{
+        "text":"Main Text",
+        "tags":["0101", "10101"]
+    }
+}
+app = QApplication([])
+notes_win = QWidget()
+notes_win.setWindowTitle('-Умные заметки')
+notes_win.resize(900,600)
+list_notes = QListWidget()
+list_notes_label = QLabel('Список Дел')
+button_note_create = QPushButton('Создать заметку')
+button_note_del = QPushButton('Удалить заметку')
+button_note_save = QPushButton('Сохранить заметку')
+list_tags = QListWidget()
+list_tags_label = QLabel('Список тегов')
+button_tag_add = QPushButton('Добавить тег')
+button_tag_del = QPushButton('Удалить тег')
+button_tag_search = QPushButton('Поиск по тегу')
+field_text = QTextEdit()
+field_tag = QLineEdit()
+field_tag.setPlaceholderText('Введите тег...')
+layout_notes = QHBoxLayout()
+col_1 = QVBoxLayout()
+col_2 = QVBoxLayout()
+row_1 = QHBoxLayout()
+row_2 = QHBoxLayout()
+row_3 = QHBoxLayout()
+row_4 = QHBoxLayout()
+col_1.addWidget(field_text)
+col_2.addWidget(list_notes_label)
+col_2.addWidget(list_notes)
+row_1.addWidget(button_note_create)
+row_1.addWidget(button_note_del)
+row_2.addWidget(button_note_save)
+col_2.addLayout(row_1)
+col_2.addLayout(row_2)
+col_2.addWidget(list_tags_label)
+col_2.addWidget(list_tags)
+col_2.addWidget(field_tag)
+row_3.addWidget(button_tag_add)
+row_3.addWidget(button_tag_del)
+row_4.addWidget(button_tag_search)
+col_2.addLayout(row_3)
+col_2.addLayout(row_4)
+layout_notes.addLayout(col_1, stretch=2)
+layout_notes.addLayout(col_2, stretch=1)
+notes_win.setLayout(layout_notes)
+field_text.setFontPointSize(12)
+field_text.setFontFamily("monospace")
+def show_note():
+    key = list_notes.selectedItems()[0].text()
+    field_text.setText(notes[key]['text'])
+    list_tags.clear()
+    list_tags.addItems(notes[key]['tags'])
+def add_note():
+    note_name, ok = QInputDialog.getText(notes_win, "Add Note", "Note Name:") 
+    if ok and note_name:
+        notes[note_name] = {'text' : "", 'tags' : []}
+        list_notes.addItem(note_name)
+def save_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        notes[key]['text'] = field_text.toPlainText()
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes, file, sort_keys = True)
+def del_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        del notes[key]
+        list_notes.clear()
+        list_tags.clear()
+        field_text.clear()
+        list_notes.addItems(notes)
+        with open("notes_data.json", "w") as file:
+            json.dump(notes, file, sort_keys=True)
+def add_tag():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        tag = field_tag.text()
+        if not tag in notes[key]['tags']:
+            notes[key]['tags'].append(tag)
+            list_tags.addItem(tag)
+            field_tag.clear()
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes, file, sort_keys = True)
+def del_tag():
+    if list_tags.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        tag = field_tag.text()
+        if not tag in notes[key]['tags']:
+            notes[key]['tags'].append(tag)
+            list_tags.addItem(tag)
+            field_tag.clear()
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes, file, sort_keys = True)
+button_tag_add.clicked.connect(add_tag)
+button_note_del.clicked.connect(del_note)
+button_note_save.clicked.connect(save_note)
+button_note_create.clicked.connect(add_note)
+list_notes.itemClicked.connect(show_note)
+with open("notes_data.json", "r") as file:
+    notes = json.load(file)
+list_notes.addItems(notes)
+notes_win.show()
+app.exec_()
